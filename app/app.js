4d58e264c14703ba45ff14c80eb3422efeab3598
OneManDP <breul@umass.edu>
2016-11-12 13:26:44 -0500

    converted settings to JS

--- app/app.js
+++ app/app.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Navbar from './components/navbar'
-import {Router, Route, browserHistory } from 'react-router'
+import Navbar from './components/navbar';
+import {Link,Router, Route, browserHistory } from 'react-router';
 
 class Home extends React.Component {
   render() {
@@ -79,7 +79,98 @@ class CourseHistory extends React.Component {
 class Settings extends React.Component {
   render() {
     return (
-    <p>Settings</p>
+<div className="container">
+  <div className="row">
+    <div className="col-md-1"></div>
+    <div className="col-md-2 acct-sidebar">
+      <button type="button" className="btn navbar-btn btn-default pull-right">
+        <span className="glyphicon glyphicon-arrow-left"> Settings Panel</span>
+      </button>
+      <br /><br />
+      <hr />
+      <div className="row">
+        <div className="col-md-7">
+          <button type="button" className="btn navbar-btn btn-default pull-left btn-block">
+            <span>Account</span>
+          </button>
+          <br />
+          <button type="button" className="btn navbar-btn btn-default pull-left btn-block">
+            <span>Preferences</span>
+          </button>
+          <hr />
+          <Link to={"/about"}>
+        <button type="button" className="btn navbar-btn btn-default pull-left btn-block">
+          <span>FAQ</span>
+        </button>
+          </Link>
+          <hr />
+          <button type="button" className="btn navbar-btn btn-default pull-left btn-block">
+            <span>Apply Changes</span>
+          </button>
+          <hr />
+          <button type="button" className="btn navbar-btn btn-default pull-left btn-block">
+            <span>Cancel</span>
+          </button>
+        </div>
+      </div>
+    </div>
+
+    <div className="col-md-6 acct-body">
+      <div className="row">
+        <div className="col-md-12">
+        <span className="bdy-hdr">Preferences</span>
+        <hr />
+      </div>
+    </div>
+    <div className="row">
+      <div className="col-md-12">
+      <span className="bdy-hdr-sub">On Startup</span>
+      <div className="checkbox">
+        <label><input type="checkbox" />Open Online Tracking Tool</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Open Settings</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Open Course History</label>
+      </div>
+      <hr />
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-md-12">
+      <span className="bdy-hdr-sub">Font Size</span>
+      <div className="checkbox">
+        <label><input type="checkbox" />Small</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Standard</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Large</label>
+      </div>
+      <hr />
+    </div>
+  </div>
+  <div className="row">
+    <div className="col-md-12">
+      <span className="bdy-hdr-sub">Themes</span>
+      <div className="checkbox">
+        <label><input type="checkbox" />Standard</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Dark</label>
+      </div>
+      <div className="checkbox">
+        <label><input type="checkbox" />Greyscale</label>
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+</div>
+
+
     );
   }
 }
@@ -101,7 +192,7 @@ ReactDOM.render((
       <Route path="/about" component={About} />
       <Route path="/savepage" component={SavePage} />
       <Route path="/coursehistory" component={CourseHistory} />
-      <Route path="/Settings" component={Settings} />
+      <Route path="/settings" component={Settings} />
     </Route>
   </Router>
 ),document.getElementById('app'));
