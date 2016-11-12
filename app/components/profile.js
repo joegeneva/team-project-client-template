8e29c06d33973692b17a9aa1d3e0e2b3f0a2e4a3
OneManDP <breul@umass.edu>
2016-11-12 16:16:14 -0500

    added Profile page to navbar,app.js,index.html

--- app/components/profile.js
+++ app/components/profile.js
@@ -1,47 +1,84 @@
 import React from 'react';
-import FormSub from './formsubmit';
-
+import {Link} from 'react-router';
 export default class Profile extends React.Component{
   render(){
     return(
       <div className="container">
         <div className="row">
-          <div className="col-md-3">
-            <div id="lefsid" className="about-settings">
-              <button type="button" className="btn navbar-btn btn-default">
-                <span> About</span>
-              </button>
-              <br />
-              <button type="button" className="btn navbar-btn btn-default">
-                <span> FAQ</span>
-              </button>
-              <br />
-              <button type="button" className="btn navbar-btn btn-default">
-                <span> Helpful Links</span>
-              </button>
-              <br />
-              <button type="button" className="btn navbar-btn btn-default">
-                <span> Contact Us</span>
-              </button>
+          <div className="col-xs-4">
+            <img src="../build/img/profile.jpeg" alt="profile image" style="width: 210px;height:210px;" />
+
           </div>
+          <div class ="col-xs-3 main-bdy">
+            <span className="category">Full Name: </span>
+            <p></p>
+            <span className="category">University ID: </span>
+            <p></p>
+            <span className="category">Major(s): </span>
+            <p></p>
+            <span className="category">Minor(s): </span>
+            <p></p>
+            <span className="category">Grad Date: </span>
+            <p></p>
+            <span className="category">Username: </span>
+            <p></p>
+            <hr />
           </div>
-          <div className="col-md-9 about-canvas">
-                  <h2>About</h2>
-                  <p className="about-subtext">This is a web app designed by six CS326 students here at UMass in order to help you plan your future! You can easily visualize what classes you still need to take and how far away from a minor you might be. You can also achieve a nifty sense of accomplishment when you complete a class and slowly move closer to those glorious upper-level electives like Web Programming. Good luck!</p>
+          <div class ="col-xs-3 main-bdy2">
+            <span className="data">Sample User</span>
+            <p></p>
+            <span className="data">12345678</span>
+            <p></p>
+            <span className="data">Computer Science</span>
+            <p></p>
+            <span className="data">Mathematics</span>
+            <p></p>
+            <span className="data">May, 2018</span>
+            <p></p>
+            <span className="data">suser@umass.edu</span>
+            <p></p>
+            <hr />
+          </div>
+        </div>
+        <div className="row">
+          <div className="col-xs-4">
+            <div className="row sidebar-box">
+              <div className="sidebar">
+                  <button type="button" className="btn navbar-btn btn-default">
+                    <span> Saved Graphs</span>
+                  </button>
                   <br />
-                  <p className="about-subtext">After a subtle line break, Im back to bring you a quick little tutorial of this app! Go to the main page, select which major/minor you would like to view, and take a look at the path to your favorite classes. Prerequisites are denoted by arrows. Make sure to check off the classes you have already taken so that you can easily see which ones are now open to you and which double major or minor you could complete! Check out the settings to customize your experience.</p>
-                  <h2>FAQ</h2>
-                    <h5 className="about-subtitle">How do I view my tracking form?</h5>
-                    <p className="about-subtext">Clicking on the "Main" button in the top left will take you there!</p>
-                    <h5 className="about-subtitle">Why are there no good questions in this FAQ?</h5>
-                    <p className="about-subtext">Well, quite frankly, nobody has asked any questions yet! So no questions are frequently asked at this point. Maybe someday. Making this line as long as possible is also my way of testing the borders of this very same web app! I hope that answers your question, if not then please contact us below!</p>
-                  <h2>Helpful Links</h2>
-                    <h5 className="about-subtext"><a href="https://moodle.umass.edu/">Moodle</a></h5>
-                    <h5 className="about-subtext"><a href="https://www.spire.umass.edu/">Spire</a></h5>
-                    <h5 className="about-subtext"><a href="http://www.umass.edu/">UMass</a></h5>
-                  <h2>Contact Us</h2>
-                  <FormSub />
+                  <button type="button" className="btn navbar-btn btn-default">
+                    <span> Course History</span>
+                  </button>
+                  <br />
+                  <button type="button" className="btn navbar-btn btn-default">
+                    <span> About</span>
+                  </button>
+                  <br />
+                  <button type="button" className="btn navbar-btn btn-default">
+                    <span> blank</span>
+                  </button>
+              </div>
+            </div>
           </div>
+          <div className="col-xs-5">
+              <div className="input-group">
+                <input type="text" className="form-control fb-search" placeholder="Old Password" />
+              </div>
+              <br />
+              <div className="input-group">
+                <input type="text" className="form-control fb-search" placeholder="New Password" />
+              </div>
+              <br />
+              <div className="input-group">
+                <input type="text" className="form-control fb-search" placeholder="Confirm New Password" />
+              </div>
+              <br />
+          <button type="button" className="btn btn-default">
+            <span>Change Password</span>
+          </button>
+        </div>
         </div>
       </div>
     )
