883bb1f7332cf8f31f99639d4261f06b669c545e
Geon Yoon <ssy01013@gmail.com>
2016-11-13 14:50:19 -0500

    updating save pages

--- app/components/savePageItem.js
+++ app/components/savePageItem.js
@@ -11,6 +11,8 @@ export default class SavePageItem extends React.Component {
       <div className="row panel-body box-line">
         <button className="btn btn-default pull-left">{this.props.name}</button>
         <a className="pull-right">{this.props.time}</a>
+        <br />
+        <hr className="black" />
       </div>
     )
   }
